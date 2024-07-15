24954aeb524ea9794fde44c1fe133e3eabd38307
Edgard Alas <edgardalasu@gmail.com>
2024-07-15 13:56:28 -0600

    chore: update global CSS and Tailwind config for new theme colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,18 @@ const config: Config = {
 			screens: {
 				'2xl': '1440px',
 			},
+			colors: {
+				theme: {
+					'blue-dark-intro': 'hsl(217, 28%, 15%)',
+					'blue-dark-main': 'hsl(218, 28%, 13%)',
+					'blue-dark-footer': 'hsl(216, 53%, 9%)',
+					'blue-dark-testimonials': 'hsl(219, 30%, 18%)',
+					'accent-cyan': 'hsl(176, 68%, 64%)',
+					'accent-blue': 'hsl(198, 60%, 50%)',
+					'accent-red': 'hsl(0, 100%, 63%)',
+					'neutral-white': 'hsl(0, 0%, 100%)',
+				},
+			},
 		},
 	},
 	plugins: [],
